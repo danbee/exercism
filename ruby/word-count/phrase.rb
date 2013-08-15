535e7c8428eff6ced5a2b4a895be694bc2db8d29
Dan Barber <danbee@gmail.com>
2013-08-15 19:55:12 +0100

    Second iteration of Word Count.

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -4,8 +4,7 @@ class Phrase
   end
 
   def word_count
-    words.each_with_object({}) do |word, word_counts|
-      word_counts[word] = 0 if word_counts[word].nil?
+    words.each_with_object(Hash.new(0)) do |word, word_counts|
       word_counts[word] += 1
     end
   end
@@ -13,10 +12,6 @@ class Phrase
   private
 
   def words
-    sanitised_text.split(' ')
-  end
-
-  def sanitised_text
-    @text.downcase.gsub(/[^a-z0-9]+/, ' ')
+    @text.downcase.scan(/[a-z0-9]+/)
   end
 end
