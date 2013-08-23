8f69fb7df537f5cb7191119bc32f10fc962cd484
Dan Barber <dan@new-bamboo.co.uk>
2013-08-23 15:56:28 +0100

    Refactoring Anagram.

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,19 +1,21 @@
 class Anagram
   def initialize(word)
-    @word = word.downcase
+    @word = Word.new(word.downcase)
   end
 
   def match(words)
-    words.map(&:downcase).each_with_object([]) do |word, matches|
+    words.map { |word| Word.new(word.downcase) }.each_with_object([]) do |word, matches|
       matches << word if word.is_anagram_of?(@word)
     end
   end
 end
 
-class String
+class Word < String
   def is_anagram_of?(word)
     self != word && sort_chars == word.sort_chars
   end
+
+  private
 
   def sort_chars
     chars.sort.join
